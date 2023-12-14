beaa7be39394a934afc6506abe0182feb0b275b8
Cris Mihalache <f3rno64@gmail.com>
2023-12-15 00:05:14 +0200

    fix: getStartDate and getEndDate

--- src/utils/dates/get_end_date.ts
+++ src/utils/dates/get_end_date.ts
@@ -3,10 +3,10 @@ import _isDate from 'lodash/isDate'
 const getEndDate = (date?: Date): Date => {
   const d = _isDate(date) ? new Date(+date) : new Date()
 
-  d.setUTCHours(23)
-  d.setUTCMinutes(59)
-  d.setUTCSeconds(59)
-  d.setUTCMilliseconds(999)
+  d.setHours(23)
+  d.setMinutes(59)
+  d.setSeconds(59)
+  d.setMilliseconds(999)
 
   return d
 }
--- src/utils/dates/get_start_date.ts
+++ src/utils/dates/get_start_date.ts
@@ -3,10 +3,10 @@ import _isDate from 'lodash/isDate'
 const getStartDate = (date?: Date): Date => {
   const d = _isDate(date) ? new Date(+date) : new Date()
 
-  d.setUTCHours(0)
-  d.setUTCMinutes(0)
-  d.setUTCSeconds(0)
-  d.setUTCMilliseconds(0)
+  d.setHours(0)
+  d.setMinutes(0)
+  d.setSeconds(0)
+  d.setMilliseconds(0)
 
   return d
 }
