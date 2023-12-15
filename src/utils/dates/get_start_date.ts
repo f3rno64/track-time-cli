f0691b9aff1945ccdbd25ec714d94c5df0afda4e
Cris Mihalache <f3rno64@gmail.com>
2023-12-15 16:51:53 +0200

    refactor: unecessary date init

--- src/utils/dates/get_start_date.ts
+++ src/utils/dates/get_start_date.ts
@@ -1,7 +1,7 @@
 import _isDate from 'lodash/isDate'
 
 const getStartDate = (date?: Date): Date => {
-  const d = _isDate(date) ? new Date(+date) : new Date()
+  const d = _isDate(date) ? date : new Date()
 
   d.setHours(0)
   d.setMinutes(0)
