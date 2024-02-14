dd34ba47c3056ee9d79e0499ad4bebd0b195d745
Cris Mihalache <f3rno64@gmail.com>
2024-02-14 06:51:38 +0200

    refactor: make sheet names red

--- src/color/sheet.ts
+++ src/color/sheet.ts
@@ -1,5 +1,5 @@
 import colors from 'colors'
 
-const clSheet = (input: string): string => colors.yellow.bold(input)
+const clSheet = (input: string): string => colors.red.bold(input)
 
 export default clSheet
