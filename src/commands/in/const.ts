8e37978a8f6499d2870b1157da4df07fb6e1b589
Cris Mihalache <f3rno64@gmail.com>
2024-02-21 22:29:31 +0200

    fix(in): alter description arg requirement to fix --help

--- src/commands/in/const.ts
+++ src/commands/in/const.ts
@@ -2,7 +2,7 @@ import { AtOption, DescriptionOption, HelpOption, setup } from '../../options'
 
 export const CONFIG = {
   aliases: ['i'],
-  command: 'in <description..>',
+  command: 'in [description..]',
   describe: 'Check in to a time sheet',
   builder: setup.bind(null, [AtOption, DescriptionOption, HelpOption])
 }
